7f1080a94b5f4a1e02558ea5e853608aeb17fcbb
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-03-05 15:46:35 -0500

    changing a few things with my server.js

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -8,7 +8,7 @@ module.exports = (app) => {
 
     app.get('/notes', (req, res) => res.json(tableData));
 
-    app.post('/api/tables', (req, res) => {
+    app.post('/api/notes', (req, res) => {
         // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
         // It will do this by sending out the value "true" have a table
         // req.body is available since we're using the body parsing middleware
