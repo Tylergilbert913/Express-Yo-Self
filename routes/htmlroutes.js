b427e4696a48cfed6dd3c6813a85ed019a99d100
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-03-08 18:21:42 -0500

    adding the final piece

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,23 +1,18 @@
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-
+const fs = require("fs");
 const path = require('path');
-
-// ROUTING
 
 module.exports = (app) => {
   
   app.get('/notes', (req, res) => {
 
-    res.sendFile(path.join(__dirname, '.assets/notes.html'));
+    res.sendFile(path.join(__dirname, './assets/notes.html'));
 });
 
-// when any other route is accessed display index.html
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './asset/index.html'));
 });
 
-// updating the json whenever something is adjusted
 const dbUpdate = () => {
     fs.writeFile('./db/db.json', JSON.stringify(notes, '\t'), err => {
         if (err) throw err;
