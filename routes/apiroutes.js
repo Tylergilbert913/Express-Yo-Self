1e6a5ef0737df1d42a6cb85a50ab3dd4ce693587
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-03-08 22:06:28 -0500

    added an app.delete function

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -9,8 +9,8 @@ module.exports = (app) => {
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
       if (err) throw err;
       res.send(data);
-    })
-  })
+    });
+  });
 
   
   app.post('/api/notes', (req, res) => {
@@ -25,6 +25,18 @@ module.exports = (app) => {
         if (err) throw err;
         return true;
       });
-    })
-  })}
+    });
+
+    app.delete('/api/notes/:id', (req, res) => {
+      let notes = fs.readFileSync(path.join(__dirname, './db/db.json'), 'utf-8');
+      notes = JSON.parse(notes);
+      notes = notes.filter(function(note) {
+          if (req.params.id != note.id)
+          return true;
+          else return false;
+      });
+      fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(notes));
+      res.json(notes);
+  });
+  })};
 
