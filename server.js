d6cf1958b95c592fe5f785e193010fbeefc395ca
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-03-10 14:46:21 -0500

    added comments for server.js

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
+// variable depedencies
 const express = require("express");
-const path = ("path");
+const path = require("path");
 const fs = require("fs");
-
-
+// calling the express
 const app = express();
 
-
+// Port setup
 const PORT = process.env.PORT || 3253;
-
+// middleware
 app.use(express.static('assets'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+// importing the routes
 require('./routes/apiroutes')(app);
 require('./routes/htmlroutes')(app);
 
-
+// assigning the listener for the PORT
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
 });
