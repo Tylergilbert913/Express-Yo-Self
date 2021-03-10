e1d5bf218dfa85dc8f1bb70aba9e9c3a01915764
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-03-10 13:49:57 -0500

    added commenting to the html routes

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -4,18 +4,18 @@ const app = express;
 
 module.exports = (app) => {
   
-  
+  // this routes to the main index page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './assets/index.html'));
 });
 
+// this toutes to the notes page
 app.get('/notes', (req, res) => {
 
   res.sendFile(path.join(__dirname, '../assets/notes.html'));
 });
 
-
-
+// this is if theres no matching route, then its rerouted to the main index page
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './assets/index.html'));
 });
